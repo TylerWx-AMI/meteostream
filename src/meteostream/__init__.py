609c366f9a13f0ff89746f4670298526f827fd71
TylerWx-AMI <tylerradebaugh@amiwx.com>
2025-01-14 21:17:23 +0000

    update init

--- src/meteostream/__init__.py
+++ src/meteostream/__init__.py
@@ -1,7 +1,9 @@
 from .hycom import HycomClient
+from .gefs import GefsClient
 from .noaa_crw import get_latest_CRW, latlon_point_data, latlon_grid_data
 
 __all__ = ["HycomClient", 
+           "GefsClient",
            "get_latest_CRW",
            "latlon_point_data",
            "latlon_grid_data"
