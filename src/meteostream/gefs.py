2b44f36cd9595b5c9c2eda2d27c8de7aace30a5c
TylerWx-AMI <tylerradebaugh@amiwx.com>
2025-02-05 06:09:54 +0000

    feat: GEFS update

--- src/meteostream/gefs.py
+++ src/meteostream/gefs.py
@@ -13,12 +13,9 @@ import numpy as np
 import pygrib
 import xarray as xr
 import pandas as pd
-
+import dask.array 
 
 from .function_tools import monitor_resources
-
-# Forecast hours
-FORECAST_HOURS = [f"{i:03d}" for i in range(0, 243, 3)]
 
 # Set datetime logic
 current_time = datetime.now(tz=timezone.utc)
@@ -44,12 +41,12 @@ elif run_time == "12" and current_hour < 1:
 else:
     run_date = current_time.strftime("%Y%m%d")
 
-# Define URL paths based on datetime logic
-base_url = f"https://nomads.ncep.noaa.gov/pub/data/nccf/com/gens/prod/gefs.{run_date}/{run_time}/wave/gridded/"
-file_urls = [f"gefs.wave.t{run_time}z.prob.global.0p25.f{hour}.grib2" for hour in FORECAST_HOURS]
+# NOAA NOMDAS SERVER FORECAST (TAU) SCHEMA
+SEGMENT_ONE = range(0, 243, 3) 
+SEGMENT_TWO = range(246, 390, 6)
+SERVER_TAU_LIST = list(SEGMENT_ONE) + list(SEGMENT_TWO)
 
-# Combine base + file_urls 
-target_urls = [f"{base_url}{url}" for url in file_urls]
+PARAM_INDX = list(range(1, 73, 1))
 
 class GefsClient():
     """
@@ -61,6 +58,7 @@ class GefsClient():
     def __init__(self, 
                  grib_dir:str, 
                  idx_list: list = [5, 6, 7, 8, 38, 39, 40],
+                 forecast_hours: tuple =  (0, 243, 3),
                  output_filepath: str ="gefs_main.nc"):
         """
         Initialize the object
@@ -74,27 +72,56 @@ class GefsClient():
         idx_list: list[int]
             list of index values to parse the .grib
             paramters
+        
+        forecast_hours: tuple
 
         output_filepath: str
             path to store resulting nc file
         """
 
+        # Handle kwargs
+        # Use a set to efficiently check if any index is not valid
+        invalid_indices = [i for i in idx_list if i not in PARAM_INDX]
+
+        if invalid_indices:
+            raise IndexError(f"Invalid Parameter Index value(s) passed: {invalid_indices}. Expecting any in {PARAM_INDX}")
+        
+        self.idx_list = idx_list # Init the idx list
+
+        start, end, step = forecast_hours  # Unpack the tuple
+        self.forecast_hours_list = range(start, end, step)
+        self.forecast_hours = sorted(f"{i:03d}" for i in self.forecast_hours_list)
+
+        # Check for invalid forecast hours
+        invalid_hours = [hour for hour in self.forecast_hours_list if hour not in SERVER_TAU_LIST]
+
+        if invalid_hours:
+            raise ValueError(f"Invalid forecast hours: {invalid_hours}. Expecting values in {SERVER_TAU_LIST}")
+        
+        
+        # Get the grib dir
         self.grib_dir = Path(grib_dir)
         
-        self.idx_list = idx_list
-        self.metadata = self.gefs_metadata()
-
+        # Set the output filepath
         self.output_filepath = output_filepath
 
-        # Add an attribute to check if downloaded
-        self.check_download = False
-
+        # Create the metadata df
+        self.metadata = self.gefs_metadata()
         # Filter the init index for the var_name
         self.filtered_df = self.metadata.loc[self.idx_list]
         self.sel_columns = self.filtered_df[['var_name', 'grb_shortName']]
-
         # Convert to a dictionary with the idx_ID as keys and (var_name, grb_shortName) as values
         self.var_dict =self.sel_columns.to_dict('index')
+
+        # Define URL paths based on current datetime logic
+        base_url = f"https://nomads.ncep.noaa.gov/pub/data/nccf/com/gens/prod/gefs.{run_date}/{run_time}/wave/gridded/"
+        file_urls = [f"gefs.wave.t{run_time}z.prob.global.0p25.f{hour}.grib2" for hour in self.forecast_hours]
+
+        # Combine base + file_urls 
+        self.target_urls = [f"{base_url}{url}" for url in file_urls]
+
+        # Add an attribute to check if downloaded
+        self.check_download = False
 
     def download_gefs_prob_data(self):
         """
@@ -102,7 +129,7 @@ class GefsClient():
         """
     
         # Check for server status
-        for target in target_urls:
+        for target in self.target_urls:
             response = requests.get(target)
             if response.status_code == 200:
                 file_name = target.split("/")[-1]
@@ -116,166 +143,90 @@ class GefsClient():
         
         self.check_download = True
 
-    # def process_gefs(self):
-    #     """
-    #     Method to extract the index (parameters) from the native gefs
-    #     files and write to netcdf format for further processing
-    #     """
-
-    #     if not self.check_download:
-    #         self.download_gefs_prob_data()
-
-    #     # Loop through all GRIB files in the directory
-    #     for filename in sorted(os.listdir(self.grib_dir)):
-    #         if filename.startswith(f"gefs.wave"):
-    #             tau = filename[-9:-6] # Slice for the tau string       
-    #             input_filepath = os.path.join(self.grib_dir, filename)
-    #             print(f'Processing {input_filepath}...')
-                
-    #             # Use pygrib to extract data
-    #             with pygrib.open(input_filepath) as grbs:
-    #                 for target_index in self.idx_list:
-    #                         # Extract the target parameter
-    #                         grb = grbs.message(target_index)
-    #                         print(f"Target idx: {target_index}, Name:{grb.shortName}")
-
-    #                         # Find matching variable in self.var_dict
-    #                         if target_index in self.var_dict:
-    #                             values = self.var_dict[target_index]
-    #                             var_name = values['var_name']
-    #                             grb_name = values['grb_shortName']
-    #                             data = grb.values
-    #                             lats, lons = grb.latlons()
-    #                             valid_time = grb.validDate   # Valid time as datetime object
-    #                             ref_time = grb.analDate      # Reference (analysis) time as datetime object
-
-    #                             # Check if the extracted parameter matches the expected shortName
-    #                             if grb.shortName == grb_name:
-    #                                 output_filename = (
-    #                                     f"GEFS_{var_name}_{filename.split('.')[0]}_idx{target_index}_f{tau}.nc"
-    #                                 )
-    #                                 output_filepath = os.path.join(self.grib_dir, output_filename)
-
-    #                                 # Create and write to a new NetCDF file
-    #                                 # Need to pivot away from pygrib when passing
-    #                                 # to xarray as this may create memory issues 
-    #                                 # and segmentation faults
-    #                                 with Dataset(output_filepath, mode='w', format='NETCDF4') as ncfile:
-    #                                     # Define dimensions
-    #                                     ncfile.createDimension('latitude', lats.shape[0])
-    #                                     ncfile.createDimension('longitude', lons.shape[1])
-    #                                     ncfile.createDimension('valid_time', None)  # Unlimited time dimension for valid_time and ref_time
-    #                                     ncfile.createDimension('ref_time', None)
-
-
-    #                                     # Define coordinate variables
-    #                                     latitudes = ncfile.createVariable('latitude', np.float32, ('latitude',))
-    #                                     longitudes = ncfile.createVariable('longitude', np.float32, ('longitude',))
-    #                                     valid_times = ncfile.createVariable('valid_time', 'f8', ('valid_time',))
-    #                                     ref_times = ncfile.createVariable('ref_time', 'f8',('ref_time',))
-
-    #                                     # Define the data variable
-    #                                     data_var = ncfile.createVariable(var_name, np.float32, ('valid_time', 'latitude', 'longitude'))
-
-    #                                     # Write coordinate values
-    #                                     latitudes[:] = lats[:, 0]        # Latitude values
-    #                                     longitudes[:] = lons[0, :]       # Longitude values
-
-    #                                     # Use CF-compliant time units and calendar
-    #                                     time_units = "hours since 1970-01-01 00:00:00"
-    #                                     valid_times[0] = date2num(valid_time, units=time_units, calendar="standard")
-    #                                     ref_times[0] = date2num(ref_time, units=time_units, calendar="standard")
-
-    #                                     # Add units and calendar attributes for compatibility
-    #                                     valid_times.units = time_units
-    #                                     valid_times.calendar = "standard"
-    #                                     ref_times.units = time_units
-    #                                     ref_times.calendar = "standard"
-
-
-    #                                     # Write the data values
-    #                                     data_var[0, :, :] = data
-
-    #                                     # Add metadata
-    #                                     data_var.units = grb.units
-    #                                     data_var.long_name = grb.name
-    #                                     data_var.standard_name = grb.shortName
-
-    #                                     print(f"Saved to {output_filepath}")
-
-           
-    #     print('All files processed successfully!')
-
     def process_gefs(self):
         """
         Method to extract the index (parameters) from the native gefs
         files and write to netcdf format for further processing
         """
 
+        datasets = []
+
         if not self.check_download:
             self.download_gefs_prob_data()
 
         # Loop through all GRIB files in the directory
         for filename in sorted(os.listdir(self.grib_dir)):
-            if filename.startswith(f"gefs.wave"):
-                tau = filename[-9:-6] # Slice for the tau string       
+            if filename.startswith("gefs.wave"):
+                data_arrays = []
+                tau = filename[-9:-6]  # Slice for the tau string       
                 input_filepath = os.path.join(self.grib_dir, filename)
                 print(f'Processing {input_filepath}...')
                 
                 # Use pygrib to extract data
                 with pygrib.open(input_filepath) as grbs:
                     for target_index in self.idx_list:
-                            # Extract the target parameter
-                            grb = grbs.message(target_index)
-                            print(f"Target idx: {target_index}, Name:{grb.shortName}")
+                        grb = grbs.message(target_index)
+                        print(f"Target idx: {target_index}, Name: {grb.shortName}, Tau:{tau}")
 
-                            # Find matching variable in self.var_dict
-                            if target_index in self.var_dict:
-                                values = self.var_dict[target_index]
-                                var_name = values['var_name']
-                                grb_name = values['grb_shortName']
-                                data = grb.values
-                                data_3d = np.expand_dims(data, axis=0)
-                                lats, lons = grb.latlons()
-                                valid_time = grb.validDate   # Valid time as datetime object
-                                ref_time = grb.analDate      # Reference (analysis) time as datetime object
-                                df = self.metadata.loc[target_index]
+                        values = self.var_dict[target_index] # Extract the values from the dict
+                        var_name = values['var_name'] # Get the var string
+                        grb_name = values['grb_shortName'] # Get the grb_var string 
+                        data = grb.values # get the np.array data
+                        data_3d = np.expand_dims(data, axis=0) # Need to expand it to get the proper shape
+                        lats, lons = grb.latlons() # Get the lat,lon data
 
-                                # Check if the extracted parameter matches the expected shortName
-                                if grb.shortName == grb_name:
-                                    output_filename = (
-                                        f"GEFS_{var_name}_{filename.split('.')[0]}_idx{target_index}_f{tau}.nc"
-                                    )
-                                    output_filepath = os.path.join(self.grib_dir, output_filename)
+                        # Extract valid_time and ref_time for this iteration
+                        valid_time = grb.validDate
+                        ref_time = grb.analDate
 
-                                    # Create and write to a new NetCDF file
-                                    # Need to pivot away from pygrib when passing
-                                    # to xarray as this may create memory issues 
-                                    # and segmentation faults
-                                    # # Create a DataArray with time as an additional dimension
-                                    data_array = xr.DataArray(
-                                        data_3d,  
-                                        coords={
-                                            "latitude": (["latitude", "longitude"], lats),
-                                            "longitude": (["latitude", "longitude"], lons),
-                                            "valid_time": valid_time,                                            
-                                            "ref_time": ref_time
-                                        },
-                                        dims=["valid_time", "latitude", "longitude"],
-                                        name = var_name,
-                                        attrs={
-                                            "grb_shortName" : grb_name,
-                                            "grb_Param_Index": target_index,
-                                            "Description": df['parameter'],
-                                            "units": df['unit'],
-                                            "Limit": df['threshold'],
-                                        }
-                                    )
-                                    data_array.to_netcdf(output_filepath)
-                                    print(f"Saved to {output_filepath}")
+                        df = self.metadata.loc[target_index] # Metadata df
 
-        
+                        # Use dask.array for the data
+                        dask_data = dask.array.from_array(
+                            data_3d,
+                            chunks='auto'
+                        )
+
+                        if grb.shortName == grb_name:
+                            print(f"Creating DataArray for {var_name}")
+                            da = xr.DataArray(
+                                dask_data,  
+                                coords={
+                                    "latitude": (["latitude"], lats[:, 0]),
+                                    "longitude": (["longitude"], lons[0, :]),
+                                    "valid_time": [valid_time],  # Wrap valid_time to define it as a dimension
+                                    "ref_time": ref_time,
+                                },
+                                dims=["valid_time", "latitude", "longitude"],
+                                name=var_name,
+                                attrs={
+                                    "grb_shortName": grb_name,
+                                    "grb_Param_Index": target_index,
+                                    "Description": df['parameter'],
+                                    "units": df['unit'],
+                                    "limit": df['threshold'],
+                                }
+                            )
+                            data_arrays.append(da)
+                
+                # Merge the DataArrays into a Dataset for this file
+                merged_da = xr.merge(data_arrays)
+                datasets.append(merged_da)
+
+        # Concatenate all datasets along the 'valid_time' dimension
+        final_dataset = xr.concat(datasets, dim='valid_time')
+
+        # Edit some metadata 
+        del final_dataset.attrs['grb_shortName']
+        del final_dataset.attrs['units']
+        del final_dataset.attrs['limit']
+
+        final_dataset.attrs['grb_Param_Index'] = [idx for idx in self.idx_list]
+        final_dataset.attrs['Description'] = "GEFS Probability Data from NOAA NOMADS HTTPS"
+
+
         print('All files processed successfully!')
+        return final_dataset
 
     def _merge_data(self) -> xr.Dataset:
         """
