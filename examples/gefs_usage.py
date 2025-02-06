09a00b4f59b1cdb44c378c34461cd12d3f3d6309
TylerWx-AMI <tylerradebaugh@amiwx.com>
2025-02-06 00:38:01 +0000

    feat: added example usage script for calling the gefsclient

--- examples/gefs_usage.py
+++ examples/gefs_usage.py
@@ -1,5 +1,23 @@
+"""
+Example python script to run the GEFS Pipeline
+"""
+# Import the package
 from meteostream import GefsClient
 
-client = GefsClient(grib_dir="/home/tylerradebaugh/data/grib")
+# We will use the default index params 
+# and default 10-day forecast
+# Variable selection and forecast range can be 
+# edited on init of the client (read docs)
 
-client.run_GEFS_pipeline(save_file="/home/tylerradebaugh/data/netcdf/GEFS_prob.nc")
+# Create a instance of the GefsClient
+# Pass a directory to download and process grib2 files
+client = GefsClient(grib_dir="/home/vrsops/Ops-Scripts/data/grib")
+
+# Execute the pipeline method
+# Flexibilty added to not save to file 
+# set save_file = None
+# clear the gribs (default is False)
+# Or return the dataset object (set return_ds = True)
+client.run_GEFS_pipeline(save_file="/home/vrsops/Ops-Scripts/data/netcdf/GEFS_prob.nc",
+                         clear_gribs=True
+                         )
